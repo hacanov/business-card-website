d502b0af236956fbadfcbe4228d6ae9a55f22288
hacanov <123230509+hacanov@users.noreply.github.com>
2023-07-20 22:20:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('Привет мир')
+let offset = 0; // смещение от левого края
+const sliderLine = document.querSelector('.slider-line')
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 360;//offset += 360;
+    if (offset > 1080){
+        offset = 0;
+    }
+    slider.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function(){
+    offset = offset - 360;//offset += 360;
+    if (offset > 0){
+        offset = 1080;
+    }
+    slider.style.left = -offset + 'px';
+});
